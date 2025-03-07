b2e2ec8bbe33aecdd46c9fc002eefdfb919aadca
Divyesh Pratap Singh <singh_divyesh@outlook.com>
2025-03-07 18:27:06 -0500

    Added hasmap solution ofr leetcode 1679

--- Two_Pointers/1679_MaxNumberKPairs.py
+++ Two_Pointers/1679_MaxNumberKPairs.py
@@ -16,6 +16,7 @@ Explanation: Starting with nums = [1,2,3,4]:
 - Remove numbers 2 and 3, then nums = []
 There are no more pairs that sum up to 5, hence a total of 2 operations.
 '''
+# First Solution: Two Pointers
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         nums = sorted(nums)
@@ -33,6 +34,26 @@ class Solution:
                 end -= 1
         return count
 '''
-Time Complexity: O(N)
+Time Complexity: O(NLogN)
 Space Complexity: O(1)
+'''
+# Second Solution: HashMap
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        
+        numSet = {}
+        count = 0
+        for num in nums:
+            if k - num in numSet:
+                count += 1
+                if numSet[k-num] > 1:
+                    numSet[k-num] = numSet[k-num] - 1
+                elif numSet[k-num] == 1:
+                    numSet.pop(k-num)
+            else:
+                numSet[num] = numSet.get(num, 0) + 1
+        return count
+'''
+Time Complexity: O(N)
+Space Complexity: O(N)
 '''
