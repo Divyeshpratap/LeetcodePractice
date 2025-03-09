042b0fa2c0fb2c6157127c194523b4472e29a7da
Divyeshpratap <singh_divyesh@outlook.com>
2025-03-09 00:34:08 -0500

    added second solution for 2352

--- HashMap/2352_EqualRowColumnPairs.py
+++ HashMap/2352_EqualRowColumnPairs.py
@@ -8,6 +8,7 @@ Output: 1
 Explanation: There is 1 equal row and column pair:
 - (Row 2, Column 1): [2,7,7]
 '''
+# First Solution
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         rowDict = {}
@@ -25,4 +26,22 @@ class Solution:
 '''
 Time Complexity: O(rows*columns)
 Space Complexity: O(rows*columns)
+'''
+#Second Solution
+class Solution:
+    def equalPairs(self, grid: List[List[int]]) -> int:
+
+        count = 0
+        for row in grid:
+            for col_index in range(len(grid[0])):
+                column = []
+                for row_index in range(len(grid)):
+                    column.append(grid[row_index][col_index])
+                if row == column:
+                    count += 1
+
+        return count
+'''
+Time Complexity: O(n^3)
+Space Complexity: O(n)
 '''
