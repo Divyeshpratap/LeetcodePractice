b225111a67e1f85ca9a33ef4d31a3cf5a8fe2a2d
Divyesh Pratap Singh <singh_divyesh@outlook.com>
2025-03-10 20:38:58 -0400

    added DFS solution also for leetcode 700

--- binarySearchTree/700_searchBinarySearchTree.py
+++ binarySearchTree/700_searchBinarySearchTree.py
@@ -5,12 +5,14 @@ Find the node in the BST that the node's value equals val and return the subtree
 Input: root = [4,2,7,1,3], val = 2
 Output: [2,1,3]
 '''
+## BFS Solution
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if root is None:
@@ -28,6 +30,25 @@ class Solution:
                 if node.right is not None:
                     queue.append(node.right)
         return
+'''
+Time Complexity: O(N)
+Space Complexity: O(N)
+'''
+## DFS Solution:
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root: return None
+
+        def dfs(node):
+            if not node: return
+            if node.val == val:
+                return node
+            else:
+                found_node = dfs(node.left) or dfs(node.right)
+                
+            return found_node
+
+        return dfs(root)
 '''
 Time Complexity: O(N)
 Space Complexity: O(N)
